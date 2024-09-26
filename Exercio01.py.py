98041c1f010573491ad30e9c3fde7dae3c12408a
YgorTeodoro <grupoandrade80@gmail.com>
2024-09-25 21:37:01 -0300

    Add files via upload

---
+++ Exercio01.py.py
@@ -0,0 +1,6 @@
+print("O que e python ?")
+print("Oi meu nome e python /n sou uma linguagem de programacao com codigo aberto !")
+print("Qual programa (IDE) executa o arquivo.py ?")
+print("O vscode e responsavel por executar o arquivo.py")
+print("Qual linguagem de programacao python substituiu ?")
+print("A linguagem que o python subistituiu foi a linguagem ABC !")
